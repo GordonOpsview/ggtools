14f93899f5ff083fcd59078773942c7d2316337e
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-09-21 17:10:53 +0100

    build at Thu 21 Sep 17:10:53 BST 2023

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ ls -l | grep 'r[w-]x' | grep -v 'build\.sh' | awk '{print $9}' > list
 for tool in $(cat list); do
   grep '^#.*ggtool' ./$tool &>/dev/null || echo -e "\n## ggtool" >> ./$tool
 done
-
+echo -ne "\e[94m"
 git add .
 git commit -m "build at $(date)" && git push
+echo -ne "\e[0m"
 
 echo
 echo "The ggtools are:"
