2e9e577dddcf5d9fe247aac3ffe5b8f5b05d27f5
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-09-21 11:47:47 +0100

    build at Thu 21 Sep 11:47:47 BST 2023

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 for ggtool in $(curl https://raw.githubusercontent.com/GordonOpsview/ggtools/main/list 2>/dev/null); do
-  curl -sS -o "/usr/bin/$ggtool" https://raw.githubusercontent.com/GordonOpsview/ggtools/main/$ggtool && chmod +x "/usr/bin/$ggtool"
+  if [[ ! -f "/usr/bin/$ggtool" ]] || grep '^#.*ggtool' /usr/bin/$ggtool &>/dev/null; then
+    curl -sS -o "/usr/bin/$ggtool" https://raw.githubusercontent.com/GordonOpsview/ggtools/main/$ggtool && chmod +x "/usr/bin/$ggtool"
+  fi
 done
