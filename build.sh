ac10432ca533d7dc77ac37a0cd722ad0d7cd14b9
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-09-21 11:53:47 +0100

    build at Thu 21 Sep 11:53:47 BST 2023

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+cd "${0%/*}"
+
 ls -l | grep 'r[w-]x' | grep -v 'build\.sh' | awk '{print $9}' > list
 
 git add .
 git commit -m "build at $(date)" && git push
+
+echo -e"\nThe ggtools are:"
+cat list | sed 's/^/ * /g'
+echo
