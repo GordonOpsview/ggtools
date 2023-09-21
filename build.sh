42eb4aed3e043e326de06c377bebcca50fa2ea99
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-09-21 17:19:11 +0100

    build at Thu 21 Sep 17:19:11 BST 2023

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ echo -ne "\e[0m"
 
 echo
 echo "The ggtools are:"
-star='$'\e'[1;35m * $'\e'[0m'
+star=$'\e'"[1;35m * "$'\e'"[0m"
 cat list | sed "s/^/$star/g"
 echo
 echo "To install: \"curl -s https://raw.githubusercontent.com/GordonOpsview/ggtools/main/install.sh | sudo bash -s --\""
