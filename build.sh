35b9c69c95556eabb5d5db42daf6558c6bcd11e8
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-09-22 12:45:38 +0100

    build at Fri 22 Sep 12:45:38 BST 2023

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ echo -ne "\e[0m"
 
 echo
 echo "The ggtools are:"
-star=$'\e'"[1;35m * "$'\e'"[0m"
 cat list | sed "s/^/"$'\e'"[1;35m * "$'\e'"[0m/g"
 echo
 echo "To install: \"curl -s https://raw.githubusercontent.com/GordonOpsview/ggtools/main/install.sh | sudo bash -s --\""
--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
+e=$'\e'
+if [[ "$1" == "-v" ]]; then echo "${e}[1mInstalling ggtools${e}[0m"; fi
 for ggtool in $(curl https://raw.githubusercontent.com/GordonOpsview/ggtools/main/list 2>/dev/null); do
   if [[ ! -f "/usr/bin/$ggtool" ]] || grep '^#.*ggtool' /usr/bin/$ggtool &>/dev/null; then
     curl -sS -o "/usr/bin/$ggtool" https://raw.githubusercontent.com/GordonOpsview/ggtools/main/ggtools/$ggtool && chmod +x "/usr/bin/$ggtool"
+    if [[ "$1" == "-v" ]]; then echo "${e}[1;35m * ${e}[0m $ggtool installed."; fi
   fi
 done
 echo "ggtools installed."
