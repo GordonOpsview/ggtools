08f84ca711afead71d6a296cca1907821bd422dd
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-10-16 18:42:24 +0100

    build at Mon 16 Oct 18:42:24 BST 2023

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 e=$'\e'
-baseurl="https://raw.githubusercontent.com/GordonOpsview/ggtools/main/"
+baseurl="https://raw.githubusercontent.com/GordonOpsview/ggtools/main"
 echo "${e}[1mInstalling ggtools${e}[0m"
-for ggtool in $(curl ${baseurl}/list 2>/dev/null); do
+for ggtool in $(curl "${baseurl}/list" 2>/dev/null); do
   if [[ ! -f "/usr/bin/$ggtool" ]] || grep '^#.*ggtool' /usr/bin/$ggtool &>/dev/null; then
-    curl -sS -o "/usr/bin/$ggtool" ${baseurl}/ggtools/$ggtool && chmod +x "/usr/bin/$ggtool"
+    curl -sS -o "/usr/bin/$ggtool" "${baseurl}/ggtools/$ggtool" && chmod +x "/usr/bin/$ggtool"
     echo "${e}[1;35m * ${e}[0m $ggtool installed."
   fi
 done
