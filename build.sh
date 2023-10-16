89622909acdc5b50d9ab76445ac67f7d3059cfbc
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2023-10-16 16:31:44 +0100

    build at Mon 16 Oct 16:31:44 BST 2023

--- build.sh
+++ build.sh
@@ -12,9 +12,16 @@ git add .
 git commit -m "build at $(date)" && git push
 echo -ne "\e[0m"
 
+#
+cp -f $HOME/Code/ggtools/ggtools/* /var/www/html/ggtools/ggtools/*
+cp -f $HOME/Code/ggtools/install.sh /var/www/html/ggtools/install.sh
+cp -f $HOME/Code/ggtools/list /var/www/html/ggtools/list
+sed -i 's|^baseurl=.*$|baseurl="http://127.0.0.1:10080/ggtools/"|g' /var/www/html/ggtools/install.sh
+
 echo
 echo "The ggtools are:"
 cat list | sed "s/^/"$'\e'"[1;35m * "$'\e'"[0m/g"
 echo
 echo "To install: \"curl -sL https://tinyurl.com/installggtools | sudo bash -s --\""
+echo "         or \"curl -sL http://127.0.0.1:10080/ggtools/install.sh | sudo bash -s --\""
 echo
